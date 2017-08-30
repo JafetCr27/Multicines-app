4a0e45bf201fb16d588c8faf3b2ba844d60c1efa
Jafet <jafetbarrera27@gmail.com>
2017-08-30 11:43:04 -0600

    version 2.0

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,19 @@
 angular.module("MulticinesApp")
     .controller("MainController", function($scope, $http) {
         $scope.cartelera = "En cartelera!";
+        $scope.loading = true;
         $http({
             method: 'GET',
             url: 'https://cine-appp.herokuapp.com/menu'
         }).then(function successCallback(response) {
             $scope.pelis = [];
             $scope.pelis = response.data.products;
+            $scope.loading = false;
             console.log(response.data.products);
+
         }, function errorCallback(response) {
-            console.log(response);
+            $scope.loading = false;
+
         });
 
     }).controller("confiController", function($scope) {
